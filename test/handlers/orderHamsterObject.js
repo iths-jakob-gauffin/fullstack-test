20c91dfefea3a54ba94b9c701cbec0d9057a66a5
Jakob <jakob_gauffin@hotmail.com>
2020-06-10 11:42:49 +0200

    igen nmn

--- test/handlers/orderHamsterObject.js
+++
@@ -1,9 +0,0 @@
-///// Sorterar hamsterobjektet utefter dess propertys i alfabetsordning.
-const orderHamsterObject = hamsterObject => {
-	const specificHamster = {};
-	Object.keys(hamsterObject).sort().forEach(function(key) {
-		specificHamster[key] = hamsterObject[key];
-	});
-	return specificHamster;
-};
-exports.orderHamsterObject = orderHamsterObject;
--- test/uploadDataToFirestore.js
+++
@@ -1,9 +0,0 @@
-const { db } = require('./firebase');
-
-async function getDataIntoFirestore(data) {
-	data.map(async hamsterObj => {
-		await db.collection('hamsters').doc().set(hamsterObj);
-	});
-}
-
-module.exports = { getDataIntoFirestore };
